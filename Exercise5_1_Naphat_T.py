11132414dd4cbe312b36718ef19122d6eeedb28b
KK-Project1 <kongdkong12345@gmail.com>
2021-01-28 20:16:25 +0700

    1
    Add All

---
+++ Exercise5_1_Naphat_T.py
@@ -0,0 +1,9 @@
+number1 = int(input("Fist: "))
+number2 = int(input("Second: "))
+print("--คำตอบ--")
+print(f"{number1} * {number2} = {number1 * number2}")
+print(f"{number1} / {number2} = {number1 / number2}")
+print(f"{number1} + {number2} = {number1 + number2}")
+print(f"{number1} - {number2} = {number1 - number2}")
+print("---------")
+
